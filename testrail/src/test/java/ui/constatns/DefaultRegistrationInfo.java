ff9cc30034fd53d7603bc267b4b54aa22e3668d9
Yana <rymsha.yana@mail.ru>
2023-07-18 17:29:03 +0300

    Modify allure.properties

--- testrail/src/test/java/ui/constatns/DefaultRegistrationInfo.java
+++ testrail/src/test/java/ui/constatns/DefaultRegistrationInfo.java
@@ -6,7 +6,7 @@ public class DefaultRegistrationInfo {
     public static final String DEFAULT_PHONE_NUMBER = "3456789";
     public static final String DEFAULT_COUNTRY = "Italy";
     public static final String DEFAULT_COMPANY_NAME = "TY";
-    public static final String DEFAULT_ADDRESS = "new935";
+    public static final String DEFAULT_ADDRESS = "new956";
     public static final String DEFAULT_NUMBER_OF_USERS = "1";
 
     // login info for conformation of registration:
