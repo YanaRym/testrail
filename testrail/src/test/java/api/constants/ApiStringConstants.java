40208792eaada5d124a2eb8a8c8bab33e61516de
Yana <rymsha.yana@mail.ru>
2023-07-22 09:53:33 +0300

    modify constants

--- testrail/src/test/java/api/constants/ApiStringConstants.java
+++ testrail/src/test/java/api/constants/ApiStringConstants.java
@@ -2,13 +2,9 @@ package api.constants;
 
 public class ApiStringConstants {
 
-
-
     public static final String CONTENT_TYPE = "Content-Type";
-
     public static final String JSON = "application/json";
 
     private ApiStringConstants() {
     }
 }
-
